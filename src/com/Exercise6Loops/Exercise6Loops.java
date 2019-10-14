1c1e7b9c097471086fdf4a5202e69e0dd78c0187
Cristina2194 <cristina.reyes@alumnos.udg.mx>
2019-10-14 13:08:23 -0500

    Terminar el ejercicio

--- src/com/Exercise6Loops/Exercise6Loops.java
+++ src/com/Exercise6Loops/Exercise6Loops.java
@@ -3,8 +3,42 @@ package com.Exercise6Loops;
 public class Exercise6Loops {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Constant declarartion
+		
+		// Variable declaration
+		
+		//Objects construction
+		
+		//Create a series of 1-100 1 by 1
+		for(int i=1; i<=100;i++);
+		{
+		System.out.printl(i);
+		}
+		//Create a series of 1-100 1 by 1
+		for(int i=100; i>=100;i--);
+		{
+		System.out.println(i);
+		}
+		for(int i=0;i<=100;i+2)
+		{
+	    System.out.println(i);
+		}
+		
+		System.out.printl("Display add numbers from 100 to 200");
+		for(int i=100; i<200;i++);
+		{
+			if(i%2==0)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+		System.out.println("Display just the half of the sequence 100 to 200");
+			for(int i=100;i<200;i++);
+			if(i==150)
+			{
+				break;
+			}
+		}
 	}
-
 }
